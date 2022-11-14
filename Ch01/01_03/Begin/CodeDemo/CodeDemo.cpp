fb62cba540ba942571eb4c36325e9c390111fc88
EmAchieng <emillyachieng@gmail.com>
2022-11-14 15:04:53 +0100

    initial commit

---
+++ Ch01/01_03/Begin/CodeDemo/CodeDemo.cpp
@@ -0,0 +1,13 @@
+// Learning C++ 
+// Exercise 01_03
+// Course by Eduardo Corpeño 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello C++ after a long time" <<endl;
+    // 0 means no errors in the program
+    return(0);
+}
